838949d2e5acfca50cac262082e6133aed791026
Tab_F2E <tab@Tab-F2Es-MacBook-Pro.local>
2017-08-08 09:09:08 +0800

    Refactor the project organization

--- App.js
+++ App.js
@@ -1,23 +1,3 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import App from './app/index';
 
-import BidList from './components/BidList';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <BidList />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
