85b79069b81be546c9e76a5dcf91c6337a364c1d
ivanberry <tabvim.dev@gmail.com>
2017-08-26 11:13:12 +0800

    ‘Add Saga’

---
+++ app/config/saga.js
@@ -0,0 +1,36 @@
+import { takeEvery, select, put, call } from 'redux-saga/effects';
+
+//1.initial data
+//2.swap
+//3.change currency
+import { SWAP_CURRENCY, CHANGE_BASE_CURRENCY, INIT_UPON_STATE, CONVERSION_RESULT, CONVERSION_ERROR } from '../actions/currencies';
+
+const getLatestRate = currency => fetch(`http://api.fixer.io/latest?base=${currency}`);
+
+const fetchLatestConversionRates = function* (action) {
+    try {
+        let currency = action.currency;
+        if (currency === undefined) {
+            currency = yield select(state => state.currencies.baseCurrency);
+        }
+        const reponse = yield call(getLatestRate, currency);
+        const result = yield reponse.json();
+
+        if (result.error) {
+            yield put({ type: CONVERSION_ERROR, error: result.error });
+        } else {
+            yield put({ type: CONVERSION_RESULT, result });
+        }
+
+    } catch (e) {
+        yield put({ type: CONVERSION_ERROR, error: e.message });
+    }
+}
+
+const rootSaga = function* () {
+    yield takeEvery(INIT_UPON_STATE, fetchLatestConversionRates);
+    yield takeEvery(SWAP_CURRENCY, fetchLatestConversionRates);
+    yield takeEvery(CHANGE_BASE_CURRENCY, fetchLatestConversionRates);
+}
+
+export default rootSaga;
