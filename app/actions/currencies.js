31d8637eeee755c3bf009918763a3104a37ed3f7
ivanberry <tabvim.dev@gmail.com>
2017-08-26 11:16:08 +0800

    ‘Add Fixer.io Api call action’

--- app/actions/currencies.js
+++ app/actions/currencies.js
@@ -2,6 +2,10 @@ export const SWAP_CURRENCY = 'SWAP_CURRENCY';
 export const CHANGE_CURRENCY_AMOUNT = 'CHANGE_CURRENCY_AMOUNT';
 export const CHANGE_BASE_CURRENCY = 'CHANGE_BASE_CURRENCY';
 export const CHANGE_QUOTE_CURRENCY = 'CHANGE_QUOTE_CURRENCY';
+export const INIT_UPON_STATE = 'INIT_UPON_STATE';
+
+export const CONVERSION_RESULT = 'CONVERSION_RESULT';
+export const CONVERSION_ERROR = 'CONVERSION_ERROR';
 
 export const swapCurrency = () => ({
     type: SWAP_CURRENCY,
@@ -20,4 +24,8 @@ export const changeBaseCurrency = currency => ({
 export const changeQuoteCurrency = currency => ({
     type: CHANGE_QUOTE_CURRENCY,
     currency, 
+});
+
+export const getInitialConversions = () => ({
+    type: INIT_UPON_STATE,
 });
